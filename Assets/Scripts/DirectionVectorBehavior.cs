8bda20cecbf65f7c92466de6a5327804e5912292
Andrés Quilodrán <andres.caro.q@gmail.com>
2020-11-21 14:41:03 -0300

    Assignment 1 Part 1 ready

---
+++ Assets/Scripts/DirectionVectorBehavior.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DirectionVectorBehavior : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    private void OnDrawGizmos()
+    {
+        DrawDirectionVectorToward(target.position);
+    }
+
+    private void DrawDirectionVectorToward(Vector2 targetPosition)
+    {
+        Handles.color = Color.white;
+
+        Vector2 startingPosition = transform.position;
+        Vector2 directionVector = (targetPosition - startingPosition).normalized;
+        Vector2 endingPosition = directionVector + startingPosition;
+
+        Gizmos.DrawLine(startingPosition, endingPosition);
+    }
+}
---
+++ Assets/Scripts/RadialDetectorBehavior.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class RadialDetectorBehavior : MonoBehaviour
+{
+    [Range(0, 3)] public float radius = 1f;
+    [SerializeField] private Transform secondPoint;
+
+    private void OnDrawGizmos()
+    {
+        DrawRadiusRange(secondPoint);
+    }
+
+    private void DrawRadiusRange(Transform point)
+    {
+        Handles.color = GetRadiusColor(point);
+        Handles.DrawWireDisc(transform.position, Vector3.forward, radius);
+    }
+
+    private Color GetRadiusColor(Transform point)
+    {
+        return GetDistanceBetween(transform.position, point.position) <= radius * radius ? Color.green : Color.red;
+    }
+
+    private static double GetDistanceBetween(Vector2 transformPosition, Vector2 point)
+    {
+        return (point - transformPosition).sqrMagnitude;
+    }
+}
