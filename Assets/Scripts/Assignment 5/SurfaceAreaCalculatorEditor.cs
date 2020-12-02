a50f1cf3af75f22385d51bbad1180253d3151d28
Andrés Quilodrán <andres.caro.q@gmail.com>
2020-12-02 17:11:38 -0300

    Add Assignment 5 bonus and small refactor

--- Assets/Scripts/Assignment 5/SurfaceAreaCalculatorEditor.cs
+++ Assets/Scripts/Assignment 5/SurfaceAreaCalculatorEditor.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SurfaceAreaCalculatorEditor : MonoBehaviour
 {
@@ -8,6 +7,50 @@ public class SurfaceAreaCalculatorEditor : MonoBehaviour
     private void OnDrawGizmos()
     {
         float meshArea = AreaCalculator(mesh);
+        float meshVolume = VolumeCalculator(mesh);
+        //Debug.Log(meshVolume);
+    }
+
+    private static float VolumeCalculator(Mesh mesh)
+    {
+        if (mesh == null)
+        {
+            return 0;
+        }
+
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        float totalMeshVolume = 0f;
+
+        for (int i = 0; i < TriangleAmountIn(mesh); i++)
+        {
+            int baseIndex = i * 3;
+            Vector3 first = vertices[triangles[baseIndex]];
+            Vector3 second = vertices[triangles[baseIndex + 1]];
+            Vector3 third = vertices[triangles[baseIndex + 2]];
+
+            Debug.DrawLine(first, second);
+            Debug.DrawLine(second, third);
+            Debug.DrawLine(third, first);
+
+            totalMeshVolume += PyramidVolume(first, second, third);
+        }
+
+        return totalMeshVolume;
+    }
+
+    private static float PyramidVolume(Vector3 first, Vector3 second, Vector3 third)
+    {
+        //Debug.DrawLine(Vector3.zero, first);
+        //Debug.DrawLine(Vector3.zero, second);
+        //Debug.DrawLine(Vector3.zero, third);
+
+        Vector3 triangleNormal = Vector3.Cross(second - first, third - second);
+
+        float parallelepipedVolume = Vector3.Dot(first, triangleNormal);
+        //Debug.DrawLine(Vector3.zero, triangleNormal * parallelepipedVolume, Color.green);
+
+        return Mathf.Abs(parallelepipedVolume) / 6f;
     }
 
     private static float AreaCalculator(Mesh mesh)
@@ -23,19 +66,19 @@ public class SurfaceAreaCalculatorEditor : MonoBehaviour
 
         for (int i = 0; i < TriangleAmountIn(mesh); i++)
         {
-            totalMeshArea += TriangleArea(vertices, triangles, i);
+            int baseIndex = i * 3;
+            Vector3 first = vertices[triangles[baseIndex]];
+            Vector3 second = vertices[triangles[baseIndex + 1]];
+            Vector3 third = vertices[triangles[baseIndex + 2]];
+
+            totalMeshArea += TriangleArea(first, second, third);
         }
 
         return totalMeshArea;
     }
 
-    private static float TriangleArea(IReadOnlyList<Vector3> vertices, IReadOnlyList<int> triangles, int triangleNumber)
+    private static float TriangleArea(Vector3 first, Vector3 second, Vector3 third)
     {
-        int baseIndex = triangleNumber * 3;
-        Vector3 first = vertices[triangles[baseIndex]];
-        Vector3 second = vertices[triangles[baseIndex + 1]];
-        Vector3 third = vertices[triangles[baseIndex + 2]];
-
         Vector3 firstSide = second - first;
         Vector3 secondSide = third - second;
 
